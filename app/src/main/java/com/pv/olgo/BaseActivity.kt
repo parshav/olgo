a0a1ca63110ae297c8db445a35334356ea20dce9
Parshav Chauhan <pchauhan@clientresourcesinc.com>
2019-01-20 11:19:04 -0600

    Added bottom navigation

--- app/src/main/java/com/pv/olgo/BaseActivity.kt
+++ app/src/main/java/com/pv/olgo/BaseActivity.kt
@@ -3,21 +3,23 @@ package com.pv.olgo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import io.flutter.facade.Flutter
 
 abstract class BaseActivity : AppCompatActivity() {
 
 
-//    private val homeScreen by lazy { HomeScreen() }
-    private val homeScreen by lazy { Flutter.createFragment("route1") }
+    private val homeScreen by lazy { HomeScreen() }
+//    private val homeScreen by lazy { Flutter.createFragment("route1") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fl_container, homeScreen)
-            .commitNow()
+        savedInstanceState.onNull {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fl_container, homeScreen)
+                    .commitNow()
+        }
     }
 }
 
