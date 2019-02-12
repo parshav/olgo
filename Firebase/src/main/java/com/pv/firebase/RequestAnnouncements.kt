9a73fe0ee6ecc95a0891de782dd0e5b96cffc057
Parshav Chauhan <pchauhan@clientresourcesinc.com>
2019-02-11 20:54:12 -0600

    Scope for activity working

--- Firebase/src/main/java/com/pv/firebase/RequestAnnouncements.kt
+++ Firebase/src/main/java/com/pv/firebase/RequestAnnouncements.kt
@@ -1,5 +1,6 @@
 package com.pv.firebase
 
+import android.util.Log
 import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
@@ -21,31 +22,42 @@ internal class RequestAnnouncementsImpl(
     private val firebaseHolder: FirebaseHolder
 ) : RequestAnnouncements {
 
+    private var data: AnnouncementsResponse? = null
+
     override fun getAnnouncements(): Observable<AnnouncementsResponse> {
 
         val obs: BehaviorRelay<AnnouncementsResponse> = BehaviorRelay.create()
 
-        firebaseHolder.getDbRef(forChild = "data")
-            .addListenerForSingleValueEvent(object : ValueEventListener {
+        data?.let {
+            Log.d("PV", "This was cached")
+            obs.accept(it)
+        } ?: run {
+            Log.d("PV", "This was NOT AT ALL cached")
+            firebaseHolder.getDbRef(forChild = "data")
+                .addListenerForSingleValueEvent(object : ValueEventListener {
 
-                override fun onCancelled(p0: DatabaseError) {
-                    obs.accept(Error("Database Error").left())
-                }
-
-                override fun onDataChange(ds: DataSnapshot) {
-                    ds.getValue(AnnouncementsWrapper::class.java)?.let {
-                        obs.accept(it.right())
-                    } ?: run {
-                        obs.accept(Error("Parsing Error").left())
+                    override fun onCancelled(p0: DatabaseError) {
+                        obs.accept(Error("Database Error").left())
                     }
-                }
 
-            })
+                    override fun onDataChange(ds: DataSnapshot) {
+                        ds.getValue(AnnouncementsWrapper::class.java)?.let {
+                            obs.accept(it.right())
+                            data = it.right()
+                        } ?: run {
+                            obs.accept(Error("Parsing Error").left())
+                        }
+                    }
+
+                })
+        }
         return obs
     }
 }
 
 val announcementsRequestModule = module {
-
-    factory<RequestAnnouncements> { RequestAnnouncementsImpl(get()) }
+    //    factory<RequestAnnouncements> { RequestAnnouncementsImpl(get()) }
+    scope("main_activity_scope") {
+        RequestAnnouncementsImpl(get()) as RequestAnnouncements
+    }
 }
--- app/src/main/java/com/pv/olgo/MainActivity.kt
+++ app/src/main/java/com/pv/olgo/MainActivity.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import com.pv.base.BaseActivity
 import com.pv.base.ui
+import org.koin.android.ext.android.getKoin
 import org.koin.android.ext.android.setProperty
+import org.koin.core.scope.Scope
 
 /*val mainActivityUI = module {
     scope("main_activity") {
@@ -23,6 +25,8 @@ class MainActivity : BaseActivity() {
 
 //    override fun uiBuilder(): UI = get()
 
+    lateinit var session: Scope
+
     override fun uiBuilder() = ui {
         layout = R.layout.activity_main
         container = R.id.fl_container
@@ -32,6 +36,12 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         setProperty("selected_id", R.id.action_announcements)
+        session = getKoin().createScope("main_activity_scope")
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        session.close()
     }
 }
